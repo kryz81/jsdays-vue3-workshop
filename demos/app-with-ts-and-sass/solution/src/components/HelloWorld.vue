5fad0686baf122446ff915c7eefbadf92022b34d
kryz <k@kryz.dev>
2020-08-14 22:16:58 +0200

    create app, app with ts and sass

---
+++ demos/app-with-ts-and-sass/solution/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>
+    Hello <span>{{ msg }}</span>
+  </h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+let a: number;
+
+export default defineComponent({
+  name: "HelloWorld",
+  props: {
+    msg: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+h1 {
+  color: blue;
+
+  span {
+    color: magenta;
+  }
+}
+</style>
