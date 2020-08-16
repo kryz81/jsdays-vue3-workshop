2373cba6463a9f99b9e68d25cc2c62f54c0bc3f3
kryz <k@kryz.dev>
2020-08-16 11:17:43 +0200

    fetch data, component lifecycle

---
+++ demos/fetch-data-and-component-lifecycle/template/src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <h3>Work Tracker</h3>
+  <InfoBoard :budget="budget" :logged-sum="loggedSum" />
+  <ProjectList
+    :projects="projects"
+    @add-day="addDay"
+    @remove-day="removeDay"
+    @reset-progress="resetProgress"
+    @delete-project="deleteProject"
+  />
+  <p>
+    <button class="btn btn-success" @click="formOpened = !formOpened">
+      Add new project
+    </button>
+  </p>
+  <AddProject v-if="formOpened" @save="saveProject" />
+</template>
+
+<script>
+import InfoBoard from "./components/InfoBoard.vue";
+import AddProject from "./components/AddProject.vue";
+import ProjectList from "./components/ProjectList.vue";
+
+export default {
+  name: "App",
+  components: {
+    ProjectList,
+    AddProject,
+    InfoBoard,
+  },
+  data() {
+    return {
+      projects: [],
+      budget: 20,
+      formOpened: false,
+    };
+  },
+  computed: {
+    loggedSum() {
+      return this.projects.reduce(
+        (total, current) => (total = total + current.loggedDays),
+        0
+      );
+    },
+  },
+  methods: {
+    addDay(projectIndex) {
+      this.projects = this.projects.map((project, index) => {
+        if (projectIndex === index) {
+          return { ...project, loggedDays: project.loggedDays + 1 };
+        }
+        return project;
+      });
+    },
+    removeDay(projectIndex) {
+      this.projects = this.projects.map((project, index) => {
+        if (projectIndex === index) {
+          return { ...project, loggedDays: project.loggedDays - 1 };
+        }
+        return project;
+      });
+    },
+    resetProgress(projectIndex) {
+      this.projects = this.projects.map((project, index) => {
+        if (projectIndex === index) {
+          return { ...project, loggedDays: 0 };
+        }
+        return project;
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.highPriority {
+  background-color: cornsilk;
+}
+</style>
