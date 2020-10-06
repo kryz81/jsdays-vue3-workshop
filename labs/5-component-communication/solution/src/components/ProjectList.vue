3f1b24f6e1b760e625dbe289e5bdfdcd7d7e80ee
kryz <k@kryz.dev>
2020-10-06 11:41:41 +0200

    fix lab 5

---
+++ labs/5-component-communication/solution/src/components/ProjectList.vue
@@ -0,0 +1,90 @@
+<template>
+  <table class="table table-bordered">
+    <tr class="header">
+      <td>Project Name</td>
+      <td>Estimated Days</td>
+      <td>Logged Days</td>
+      <td>Actions</td>
+    </tr>
+    <tr
+      v-for="(project, index) in projects"
+      :key="project.projectName"
+      :class="{ highPriority: project.highPriority }"
+    >
+      <td>{{ project.projectName }}</td>
+      <td>{{ project.estimatedDays }}</td>
+      <td
+        class="estimatedDays"
+        :class="{ 'text-danger': project.loggedDays > project.estimatedDays }"
+      >
+        {{ project.loggedDays }}
+      </td>
+      <td :style="actions">
+        <button class="btn btn-primary btn-sm mr-3" @click="addDay(index)">
+          Add 1 day
+        </button>
+        <button
+          class="btn btn-primary btn-sm mr-3"
+          :disabled="project.loggedDays === 0"
+          @click="removeDay(index)"
+        >
+          Remove 1 day
+        </button>
+        <button
+          class="btn btn-danger btn-sm"
+          :disabled="project.loggedDays === 0"
+          @click="resetProgress(index)"
+        >
+          Reset logged days
+        </button>
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "ProjectList",
+  emits: ["add-day", "remove-day", "reset-progress"],
+  props: {
+    projects: {
+      type: Array,
+      default: [],
+    },
+  },
+  data() {
+    return {
+      actions: {
+        backgroundColor: "#f9f9f9",
+      },
+    };
+  },
+  methods: {
+    addDay(projectIndex) {
+      this.$emit("add-day", projectIndex);
+    },
+    removeDay(projectIndex) {
+      this.$emit("remove-day", projectIndex);
+    },
+    resetProgress(projectIndex) {
+      this.$emit("reset-progress", projectIndex);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  font-weight: bold;
+  background-color: #f5f5f5;
+}
+
+.estimatedDays {
+  font-weight: bold;
+  color: green;
+}
+
+.highPriority {
+  background-color: cornsilk;
+}
+</style>
---
+++ labs/5-component-communication/template/src/components/AddProject.vue
@@ -0,0 +1,95 @@
+<template>
+  <form @submit.prevent="saveProject" ref="form">
+    <div class="form-group">
+      <label for="projectName"
+        >Project Name<span class="required">*</span></label
+      >
+      <input
+        id="projectName"
+        class="form-control"
+        v-model.trim="form.projectName"
+      />
+    </div>
+    <div class="form-group">
+      <label for="estimatedDays"
+        >Estimated days<span class="required">*</span></label
+      >
+      <input
+        id="estimatedDays"
+        type="number"
+        class="form-control"
+        min="0"
+        v-model.number="form.estimatedDays"
+      />
+    </div>
+    <div class="form-group">
+      <label for="category">Category<span class="required">*</span></label>
+      <select id="category" class="form-control" v-model="form.category">
+        <option value="">--- Select ---</option>
+        <option v-for="option in projectCategories" :value="option" :key="option">{{
+          option
+        }}</option>
+      </select>
+    </div>
+    <div class="form-group form-check">
+      <input
+        id="highPriority"
+        type="checkbox"
+        class="form-check-input"
+        v-model="form.highPriority"
+      />
+      <label for="highPriority" class="form-check-label">High priority</label>
+    </div>
+    <div>
+      <button type="submit" class="btn btn-success">Save</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "AddProject",
+  data() {
+    return {
+      form: {
+        projectName: "",
+        estimatedDays: 0,
+        category: "",
+        highPriority: false,
+      },
+      projectCategories: [
+        "Client Project",
+        "Internal Project",
+        "Proof of Concept",
+      ],
+    };
+  },
+  methods: {
+    saveProject() {
+      this.resetForm();
+      this.formOpened = false;
+    },
+    resetForm() {
+      this.form = {
+        projectName: "",
+        estimatedDays: 0,
+        category: "",
+        highPriority: false,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  margin: 25px 0;
+  padding: 25px;
+  background-color: #f5f5f5;
+  width: 50%;
+}
+
+.required {
+  color: red;
+}
+</style>
